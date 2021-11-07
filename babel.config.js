dd3dea7b3e251a13d566299765903323193f9b2d
@aditya.pahilwani <aditya.pahilwani@gmail.com>
2021-11-07 16:58:47 +0530

    Added configs for absolute imports

--- babel.config.js
+++ babel.config.js
@@ -1,1 +1,30 @@
-module.exports = { presets: ['module:metro-react-native-babel-preset'] };
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          animated: './src/animated',
+          common: './src/common',
+          components: './src/components',
+          styles: './src/styles',
+          themes: './src/themes',
+          ui: './src/ui',
+        },
+      },
+    ],
+  ],
+};
